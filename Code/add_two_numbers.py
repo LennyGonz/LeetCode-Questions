f035a4c45707d71e35cd089c51de0117954bb5b0
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-09-05 22:55:17 -0400

    removed the print comments but added the stdout for the test run

--- Code/add_two_numbers.py
+++ Code/add_two_numbers.py
@@ -1,3 +1,9 @@
+# Definition for singly-linked list.
+# class ListNode:
+#   def __init__(self, val=0, next=None):
+#     self.val = val
+#     self.next = next
+
 def addTwoNumbers(l1, l2):
   carry = 0
   root = n = ListNode(0)
@@ -15,4 +21,39 @@ def addTwoNumbers(l1, l2):
     n = n.next
   return root.next
 
-print(addTwoNumbers([2,4,3],[5,6,4]))
+'''
+Output for test run on this example: l1 = [2,4,3] & l2 = [5,6,4]
+
+ListNode{val: 0, next: None}
+v1: 2
+l1: ListNode{val: 4, next: ListNode{val: 3, next: None}}
+****
+v2: 5
+l2: ListNode{val: 6, next: ListNode{val: 4, next: None}}
+----
+carry: 0 |||| val: 7
+n.next ListNode{val: 7, next: None}
+n: ListNode{val: 7, next: None}
+-------------------------------------------
+v1: 4
+l1: ListNode{val: 3, next: None}
+****
+v2: 6
+l2: ListNode{val: 4, next: None}
+----
+carry: 1 |||| val: 0
+n.next ListNode{val: 0, next: None}
+n: ListNode{val: 0, next: None}
+-------------------------------------------
+v1: 3
+l1: None
+****
+v2: 4
+l2: None
+----
+carry: 0 |||| val: 8
+n.next ListNode{val: 8, next: None}
+n: ListNode{val: 8, next: None}
+-------------------------------------------
+root ListNode{val: 0, next: ListNode{val: 7, next: ListNode{val: 0, next: ListNode{val: 8, next: None}}}}
+'''
