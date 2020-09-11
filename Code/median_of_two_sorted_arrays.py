3d0f1bd0c6265ad7559564e7d3b4a3d496b26b56
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-09-11 15:47:19 -0400

    removed the print statements and renamed variables

--- Code/median_of_two_sorted_arrays.py
+++ Code/median_of_two_sorted_arrays.py
@@ -3,58 +3,57 @@ Time Complexity: O(log(n+m))
 Space Complexity: O(log(n+m))
 '''
 
-def median(A, B):
-  m, n = len(A), len(B)
-  print("m:",m,"|","n:",n)
-  if m > n:
-    A, B, m, n = B, A, n, m
-    print("A:",A,"||","B",B,"||","m:",m,"||","n:",n)
-  if n == 0:
+def median(input1, input2):
+  length_of_input1, length_of_input2 = len(input1), len(input2)
+
+  if length_of_input1 > length_of_input2:
+    input1 = input2
+    input2 = input1
+    length_of_input1 = length_of_input2
+    length_of_input2 = length_of_input1 
+
+  if length_of_input2 == 0:
     raise ValueError
   
-  imin, imax, half_len = 0, m, (m + n + 1) // 2
-  print("imin:",imin,"||","imax",imax,"||","half_len",half_len)
-  while imin <= imax:
-    i = (imin + imax) // 2
-    j = half_len - i
-    print("i:",i,"||","j",j)
-    if i < m and B[j - 1] > A[i]:
-      # i is too small, must increase it
-      imin = i + 1
-      print("imin inside the if statement:",imin)
-    elif i > 0 and A[i-1] > B[j]:
-      # i is too big, must decrease it
-      imax = i - 1
-      print("imax inside of the elif statement",imax)
-    else:
-      # i is perfect
-      if i == 0:
-        max_of_left = B[j - 1]
-        print("else -> if -> max_of_left", max_of_left)
-      elif j == 0:
-        max_of_left = A[i - 1]
-        print("else -> elif -> max_of_left", max_of_left)
-      else:
-        max_of_left = max(A[i - 1], B[j - 1])
-        print("else -> else -> max_of_left:", max_of_left)
+  imin, imax, half_len = 0, length_of_input1, (length_of_input1 + length_of_input2 + 1) // 2
 
-      if (m + n) % 2 == 1:
-        print("1st return statement - returning max_of_left", max_of_left)
+  while imin <= imax:
+    input1_partition = (imin + imax) // 2
+    input2_partition = half_len - input1_partition
+
+    if input1_partition < length_of_input1 and input2[input2_partition - 1] > input1[input1_partition]:
+      # input1_partition is too small, must increase it
+      imin = i + 1
+
+    elif input1_partition > 0 and input1[input1_partition-1] > input2[input2_partition]:
+      # input1_partition is too big, must decrease it
+      imax = input1_partition - 1
+
+    else:
+      # input1_partition is perfect
+      if input1_partition == 0:
+        max_of_left = input2[input2_partition - 1]
+
+      elif input2_partition == 0:
+        max_of_left = input1[input1_partition - 1]
+
+      else:
+        max_of_left = max(input1[input1_partition - 1], input2[input2_partition - 1])
+
+      if (length_of_input1 + length_of_input2) % 2 == 1:
         return max_of_left
 
-      if i == m:
-        min_of_right = B[j]
-        print("if -> min_of_right:",min_of_right)
-      elif j == n:
-        min_of_right = A[i]
-        print("elif -> min_of_right",min_of_right)
-      else:
-        min_of_right = min(A[i], B[j])
-        print("else -> min_of_right",min_of_right)
+      if input1_partition == length_of_input1:
+        min_of_right = input2[input2_partition]
 
-      print("max_of_light",max_of_left, "||", "min_of_right",min_of_right, "||", "return=",((max_of_left+min_of_right)/2))
-      return (max_of_left + min_of_right) // 2.0
+      elif input2_partition == length_of_input2:
+        min_of_right = input1[input1_partition]
+
+      else:
+        min_of_right = min(input1[input1_partition], input2[input2_partition])
+      
+      return (max_of_left + min_of_right) / 2.0
 
 input1 = [1,3]
-input2 = [2]
+input2 = [2,4]
 print(median(input1, input2))
