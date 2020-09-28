67d3fe7c85a58576bafb473e72dbc31f2e82f0a5
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-09-27 22:32:13 -0400

    modified it

--- Code/permutations.py
+++ Code/permutations.py
@@ -3,12 +3,19 @@ def permute(nums):
   visited = set()
   # a result to return
   result = []
-  backtracking(result,visited,[],nums)
+  backtracking(result, visited, [], nums)
+  #print("result=", result)
   return result
-def backtracking(result,visited, subset, nums):
+def backtracking(result, visited, subset, nums):
+  print("----")
+  print("result", result)
+  print("visited", visited)
+  print("subset", subset)
+  print("----")
   if len(subset) == len(nums):
     result.append(subset)
-  for i in range(len(nums)): #i should 
+  for i in range(len(nums)):
+    print("i in for loop", i)
     if i not in visited:
       visited.add(i)
       backtracking(result,visited,subset+[nums[i]],nums)
