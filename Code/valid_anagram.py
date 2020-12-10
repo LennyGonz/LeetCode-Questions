9f86408327f3e59a390c54d6f751b7cbad8ef8d0
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-12-09 23:42:09 -0500

    added more comments

--- Code/valid_anagram.py
+++ Code/valid_anagram.py
@@ -4,8 +4,7 @@ def isAnagram(s, t):
   
   char_dic = {}
   
-  for index, char in enumerate(s):
-    print('index', index, '||', 'char', char)
+  for char in s:
     if char not in char_dic:
       char_dic[char] = 1
     else:
@@ -17,15 +16,20 @@ def isAnagram(s, t):
     print(item)
   
   for char in t:
+    print("current char:", char)
     if char not in char_dic:
       return False
     
     # if this letter exceeds the count of the same letter in the first word
+    # when we hit our 3rd c (ccac) the value of char_dic['c'] = 0 -- meaning we have 1 more c in string t than we did in string s
     elif char_dic[char] < 1:
       return False
 
     else:
+      print("Before -> char_dic[char]=",char_dic[char])
       char_dic[char] -= 1
+      print("After -> char_dic[char]=",char_dic[char])
+      
   
   return True
 
