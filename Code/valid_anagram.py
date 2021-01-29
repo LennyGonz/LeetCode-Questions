034cc91a48da1bfc9cc45fa0b196d9671373d47c
lennygonz <lenny.gonzaleznyc@gmail.com>
2021-01-28 23:10:00 -0500

    added clarification and removed print statements

--- Code/valid_anagram.py
+++ Code/valid_anagram.py
@@ -1,35 +1,28 @@
-def isAnagram(s, t):
-  if len(s) != len(t):
+def isAnagram(string1, string2):
+  if len(string1) != len(string2):
     return False
   
   char_dic = {}
   
-  for char in s:
+  for char in string1:
     if char not in char_dic:
       char_dic[char] = 1
     else:
       char_dic[char] += 1
-  
-  d_items = char_dic.items()
 
-  for item in d_items:
-    print(item)
-  
-  for char in t:
-    print("current char:", char)
+  for char in string2:
     if char not in char_dic:
       return False
     
     # if this letter exceeds the count of the same letter in the first word
-    # when we hit our 3rd c (ccac) the value of char_dic['c'] = 0 -- meaning we have 1 more c in string t than we did in string s
+    # this condition is only true if we have 1 or more of the current char than string1 does of that char
+    # when we hit our 3rd c (ccac) the value of char_dic['c'] = 0
+    # so on our 4th iteration char_dic['c'] = 0 -- 0 < 1 #True - therefore we return False bc the current 
     elif char_dic[char] < 1:
       return False
 
     else:
-      print("Before -> char_dic[char]=",char_dic[char])
       char_dic[char] -= 1
-      print("After -> char_dic[char]=",char_dic[char])
-      
   
   return True
 
