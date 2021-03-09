c5181a973852bbce1182718bf6b0d2ca72735e4e
lenny <lenny.gonzaleznyc@gmail.com>
2021-03-08 23:08:09 -0500

    Corrected the solution

--- Code/first_missing_positive.py
+++ Code/first_missing_positive.py
@@ -1,30 +1,24 @@
 def firstMissingPositive(nums):
   # cleaning up
   for index in range(len(nums)):
-    print("index", index)
     if nums[index] <= 0 or nums[index] > len(nums):
-      print("Cleaning Up - Before",nums[index])
       nums[index] = len(nums) + 1
-      print("Cleaning Up - After",nums[index])
-      print("******")
   
   #placing our marker
   for num in nums:
-    print("num", num)
     num = abs(num)
-    print("abs(num)",abs(num))
     if num <= len(nums) and nums[num - 1] >= 0:
       nums[num - 1] *= 1
-      print("nums[num - 1]", nums[num - 1])
-  
+
   #final step for getting the answer
   for index in range(len(nums)):
-    print("final step - index", index)
     if nums[index] > 0:
-      print("nums[index]",nums[index])
       return index + 1
   
   return len(nums) + 1
 
 input1 = [7, 8, 9, 11, 12]
 print(firstMissingPositive(input1))
+
+input2 = [1]
+print(firstMissingPositive(input2))
