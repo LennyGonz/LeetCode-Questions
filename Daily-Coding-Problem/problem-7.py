cf6b57e5a5979df5a6a2018a0b982371c69df4e7
lenny <lenny.gonzaleznyc@gmail.com>
2021-03-15 22:55:00 -0400

    Added optimal solution

--- Daily-Coding-Problem/problem-7.py
+++ Daily-Coding-Problem/problem-7.py
@@ -20,7 +20,7 @@ def num_encodings(string):
 
   return total
 
-print(num_encodings('111'))
+print(num_encodings('111')) # -> 3
 
 '''
 - If string starts with zero, then there's no valid encoding
@@ -30,4 +30,30 @@ print(num_encodings('111'))
 - If the first 2 digits form a number k that is less than or equal to 26, we can recursively count the number of encodings assuming we pick k as a letter
 
 - We can also pick the first 2 digits as a letter and count the number of encodings with this assumption
+
+In addition, the above solution is not very efficient
+Every branch calls itself recursively twice, so our runtime is O(2^n)
+We can do better by using dynamic programming
+
+All the following code does is repeat the same computation as above except starting from the base case and building up the solution
+Since each iteration takes O(1), the whole algorithm now takes O(n)
 '''
+
+from collections import defaultdict
+
+def num_encodings(s):
+  # on lookup, this hashmap returns a default value of 0 if they key doesn't exist
+  # cache[i] gives us # of ways to encode the substring s[i:]
+  cache = defaultdict(int)
+  cache[len(s)] = 1  # empty string is 1 valid encoding
+  
+  for i in reversed(range(len(s))):
+    if s[i].startswith('0'):
+      cache[i] = 0
+    elif i == len(s) - 1:
+      cache[i] = 1
+    else:
+      if int(s[i:i + 2]) <= 26:
+        cache[i] = cache[i + 2]
+      cache[i] += cache[i + 1]
+  return cache[0]
