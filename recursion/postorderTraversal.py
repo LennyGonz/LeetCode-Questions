36af91133b184edd16fd0f126ccb3a748b7edda6
lenny <lenny.gonzaleznyc@gmail.com>
2021-05-26 23:49:08 -0400

    Added BST methods of traversing recursively

---
+++ recursion/postorderTraversal.py
@@ -0,0 +1,11 @@
+def postorderTraversal(root):
+  res = []
+  helper(root, res)
+  return res
+
+  def helper(root, res):
+    if root:
+      res.append(root.val)
+      helper(root.left, res)
+      helper(root.right, res)
+
---
+++ recursion/preorderTraversal.py
@@ -0,0 +1,10 @@
+def preorderTraversal(root):
+  res = []
+  helper(root, res)
+  return res
+
+  def helper(root, res):
+    if root:
+      helper(root.left, res)
+      helper(root.right, res)
+      res.append(root.val
