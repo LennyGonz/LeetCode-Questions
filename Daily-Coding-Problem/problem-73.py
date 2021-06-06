013664b77a2ab3fab1f167765686f04f08979616
lenny <lenny.gonzaleznyc@gmail.com>
2021-06-06 17:07:27 -0400

    new solutions

---
+++ Daily-Coding-Problem/problem-73.py
@@ -0,0 +1,8 @@
+def reverse(head):
+  prev, current = None, head
+  while current is not None:
+    tmp = current.next
+    current.next = prev
+    prev = current
+    current = tmp
+  return prev
---
+++ Daily-Coding-Problem/problem-74.py
@@ -0,0 +1,6 @@
+def multi_tables(n, x):
+  count = 0
+  for i in range(1, n + 1):
+    if x % i == 0 and x / i <= n:
+      count += 1
+  return count
