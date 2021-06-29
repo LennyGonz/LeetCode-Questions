e9e306a0aed5c63b0f989c60d33973ebfda4f99d
lenny <lenny.gonzaleznyc@gmail.com>
2021-06-28 23:18:45 -0400

    some comments

--- Code/max_consecutive_ones.py
+++ Code/max_consecutive_ones.py
@@ -11,6 +11,12 @@ def findMaxConsecutiveOnes(nums):
       # reset the count variable to start recording the upcoming consecutive ones 
       count = 0
   
+  # if we iterate through another subarray and don't encounter 0s, we will not update max_count
+  # so we NEED to return max_count and count
+  # bc count could be bigger, take for example [1,1,0,1,1,1]
+  # after we encounter with the 0, we never enter the else statement again and we're done looping within nums
+  # but we never updated max_count = 2 and count = 3
+  # so as a percaution we return the max of the 2, so we're absolutely sure we return the max cosecutive ones
   return max(count, max_count)
 
 
