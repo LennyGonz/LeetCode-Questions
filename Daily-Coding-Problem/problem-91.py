8f664913db63e2a03c7a9d389e895a61b33d88f9
lenny <lenny.gonzaleznyc@gmail.com>
2021-07-05 23:13:05 -0400

    solution to 91

---
+++ Daily-Coding-Problem/problem-91.py
@@ -0,0 +1,14 @@
+'''
+At a first glance, it seems like the snippet should print out from 0 to 9. But actually, it prints 9 10 times.
+
+The problem is that the functions have closure and have access to the non-local variable i. We instead want the value of i when the functions are declared.
+
+In order to solve this issue, we should capture the value i when the funcionts are declared. This would make i a local variable inside the anonymous functions.
+'''
+
+functions = []
+for i in range(10):
+  functions.append(lambda i=i: i)
+
+for f in functions:
+  print(f())
