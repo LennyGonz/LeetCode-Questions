b7a33a94f31728d6b19c1ce83ebfc3120848fd01
lenny <lenny.gonzaleznyc@gmail.com>
2021-09-18 19:25:08 -0400

    solution to 163

---
+++ Daily-Coding-Problem/problem-163.py
@@ -0,0 +1,23 @@
+PLUS = '+'
+MINUS = '-'
+TIMES = '*'
+DIVIDE = '/'
+
+OPERANDS = [PLUS, MINUS, TIMES, DIVIDE]
+
+def rpn(expr):
+  stack = []
+  for val in expr:
+    if val in OPERANDS:
+      term1, term2 = stack.pop(), stack.pop()
+      if val == PLUS:
+        stack.append(term1 + term2)
+      elif val == MINUS:
+        stack.append(term1 - term2)
+      elif val == TIMES:
+        stack.append(term1 * term2)
+      elif val == DIVIDE:
+        stack.append(term1 / term2)
+    else:
+      stack.append(val)
+  return stack[0]
