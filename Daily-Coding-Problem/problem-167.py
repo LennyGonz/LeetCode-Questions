5afabbcf221f6c3e1ffd1fd4bb1cfec91838b32c
lenny <lenny.gonzaleznyc@gmail.com>
2021-09-22 23:51:43 -0400

    solution to 167

---
+++ Daily-Coding-Problem/problem-167.py
@@ -0,0 +1,24 @@
+def is_palindrome(word):
+  return word == word[::-1]
+
+def palindrome_pairs(words):
+  d = {}
+  for i, word in enumerate(words):
+    d[word] = i
+
+  result = []
+
+  for i, word in enumerate(words):
+    for char_i in range(len(word)):
+      prefix, postfix = word[:char_i], word[char_i:]
+      reversed_prefix, reversed_postfix = prefix[::-1], postfix[::-1]
+
+      if is_palindrome(postfix) and reversed_prefix in d:
+        if i != d[reversed_prefix]:
+          result.append((i, d[reversed_prefix]))
+
+      if is_palindrome(prefix) and reversed_postfix in d:
+        if i != d[reversed_postfix]:
+          result.append((d[reversed_postfix], i))
+
+  return result
