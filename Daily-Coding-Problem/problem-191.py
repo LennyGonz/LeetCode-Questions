7f0e4c2fc9974ee1f1572b90ade0959acf3254de
lenny <lenny.gonzaleznyc@gmail.com>
2021-10-13 13:28:22 -0400

    solution to 191

---
+++ Daily-Coding-Problem/problem-191.py
@@ -0,0 +1,12 @@
+from math import inf
+
+def non_overlapping_intervals(intervals):
+  current_end = -inf
+  overlapping = 0
+
+  for start, end in sorted(intervals, key=lambda i: i[1]):
+    if start >= current_end:
+      current_end = end
+    else:
+      overlapping += 1
+  return overlapping
