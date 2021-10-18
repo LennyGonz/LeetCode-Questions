69b22c11f6b8e14e5c58830fd8bb8f9d936c53af
lenny <lenny.gonzaleznyc@gmail.com>
2021-10-17 22:30:09 -0400

    Two Sum but Harder

--- Code/two_sum.py
+++ Code/two_sum.py
@@ -11,3 +11,33 @@ def twoSum(numarr, target):
 
 ex1 = [2, 7, 11, 15]
 print(twoSum(ex1, 26))
+
+
+def formatSongs(songDuration):
+  songs = []
+  for i in range(len(songDuration)):
+    songs.append([songDuration[i], i])
+  return songs
+
+
+
+def findSongs(rideDuration, songDurations):
+  # Write your code here
+  rideDuration -= 30
+  map = {}
+  maximum = -1
+  ans = [-1, -1]
+  
+  for i in range(len(songDurations)):
+    if songDurations[i] not in map:
+      map[rideDuration - songDurations[i]] = i
+    else:
+      if songDurations[i] > maximum or rideDuration - songDurations[i] > maximum:
+        ans[0] = map[songDurations[i]]
+        ans[1] = i
+        maximum = max(songDurations[i], rideDuration-songDurations[i])
+  
+  if ans != [-1, -1]:
+    return ans
+  else:
+    return [-1, -1]
