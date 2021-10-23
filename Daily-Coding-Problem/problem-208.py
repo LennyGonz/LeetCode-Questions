641701d3576a14698ab29c22754e6821588e2498
lenny <lenny.gonzaleznyc@gmail.com>
2021-10-22 23:39:34 -0400

    solution to 208

---
+++ Daily-Coding-Problem/problem-208.py
@@ -0,0 +1,25 @@
+class Node:
+  def __init__(self, val, next=None):
+    self.val = val
+    self.next = next
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+
+  def insert(self, data):
+    if not self.head:
+      self.head = self.tail = Node(data)
+    else:
+      tmp = Node(data)
+      tmp.next = self.head
+      self.head = tmp
+
+  def append(self, data):
+    if not self.head:
+      self.head = self.tail = Node(data)
+    else:
+      tmp = Node(data)
+      self.tail.next = tmp
+      self.tail = self.tail.next
