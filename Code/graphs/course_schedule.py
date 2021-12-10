592a10783a3d73d637655a2b926684a7b7a8c43b
lenny <lenny.gonzaleznyc@gmail.com>
2021-12-09 22:10:24 -0500

    Problem 207 from LeetCode

---
+++ Code/graphs/course_schedule.py
@@ -0,0 +1,45 @@
+# LeetCode: 207 - https://leetcode.com/problems/course-schedule/
+
+def canFinish(numCourses, prerequisites):
+  preMap = { i : [] for i in range(numCourses) }
+  
+  for course, prereq in prerequisites:
+    preMap[course].append(prereq)
+  
+  visitSet = set()
+  
+  def dfs(crs):
+    if crs in visitSet:
+      return False
+    
+    if preMap[crs] == []:
+      return True
+    
+    visitSet.add(crs)
+    
+    for pre in preMap[crs]:
+      if not dfs(pre):
+        return False
+    
+    visitSet.remove(crs)
+    preMap[crs] = []
+    
+    return True
+  
+  for crs in range(numCourses):
+    if not dfs(crs):
+      return False
+  
+  return True
+
+def main():
+  numCourses = 5
+  prerequisites = [[0,1],[0,2],[1,3],[1,4],[3,4]]
+  print(canFinish(numCourses, prerequisites))
+
+main()
+
+'''
+Time: O(N) - N nodes in the tree that we visit
+Space: O(N+M) - We use an adjancency list to store N courses and M prequeisites for those courses
+'''
