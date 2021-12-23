f9492812d09eb0f6e50d20357f4ca43649bf606c
lenny <lenny.gonzaleznyc@gmail.com>
2021-12-23 14:14:41 -0500

    solution to 253

---
+++ Daily-Coding-Problem/problem-253.py
@@ -0,0 +1,22 @@
+def zigzag(sentence, k):
+  n = len(sentence)
+  line_matrix = [[' ' for _ in range(n)] for _ in range(k)]
+
+  row = 0
+  for col, letter in enumerate(sentence):
+    line_matrix[row][col] = letter
+
+    if row == 0:
+      descending = True
+    elif row == k - 1:
+      descending = False
+
+    if descending:
+      row += 1
+    else:
+      row -= 1
+
+  for line in line_matrix:
+    print("".join(line))
+
+# This algorithm is O(k * N) in both time and space.
