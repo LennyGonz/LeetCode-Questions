9f749f180cbf36851ec323da2941db800b81f493
lenny <lenny.gonzaleznyc@gmail.com>
2022-01-09 19:10:19 -0500

    small change

--- Code/graphs/minimum_level_sum.py
+++ Code/graphs/minimum_level_sum.py
@@ -14,27 +14,27 @@ class Node:
     self.right = right
 
 def minimum_level_sum_impl(root, lookup = None, level=0):
-    if root is None:
-      return
-    if lookup.get(level) != None:
-      lookup[level] = lookup[level] + root.val
-    else:
-      lookup[level] = root.val
+  if root is None:
+    return
+  if lookup.get(level) != None:
+    lookup[level] = lookup[level] + root.val
+  else:
+    lookup[level] = root.val
 
-    minimum_level_sum_impl(root.left, lookup, level + 1)
-    minimum_level_sum_impl(root.right, lookup, level + 1)   
+  minimum_level_sum_impl(root.left, lookup, level + 1)
+  minimum_level_sum_impl(root.right, lookup, level + 1)   
 
 def minimum_level_sum(root):
-    lookup = {}
-    minimum_level_sum_impl(root, lookup, 0)
-    print(lookup)
-    return None
+  lookup = {}
+  minimum_level_sum_impl(root, lookup, 0)
+  print(lookup)
+  return None
 
 
 root = Node(
-    val=1,
-    left=Node(2),
-    right=Node(3, Node(4), Node(5))
+  val=1,
+  left=Node(2),
+  right=Node(3, Node(4), Node(5))
 )
 print(minimum_level_sum(root))
 
