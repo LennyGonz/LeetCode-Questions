4f528d1711a799bc12b0c2ec5a7a0b2d8d6bc343
lenny <lenny.gonzaleznyc@gmail.com>
2022-01-30 10:00:13 -0500

    LeetCode 200 - BFS on every land

--- Code/graphs/number_of_islands.py
+++ Code/graphs/number_of_islands.py
@@ -1,3 +1,43 @@
+'''
+LeetCode #200
+
+How do we determine if we're at an island or how do we know if it's the same island ?
+
+* draw out a diagram - starting at the first index [0][0] and visiting the 4 possible neighbors (left, right, up, and down)
+* then repeat the same process on EACH neighbor
+
+TO AVOID visiting the same island, we use a set, but we don't put the entire island in there
+We put the points/location we've visited in the set
+
+So essentially we start by performing BFS on 1 node and then BFS on its neighbors adding each node to our visited set to keep track
+
+IF we start at [0][0] we need to find out how big the island really is
+  * islands are formed by connecting 1s horizontally (left-right) and vertically (up-down)
+By checking the 4 possible directions we may find more land that creates the current island
+
+Not only do we do BFS on both recently discovered neighbors but we also need to mark them as visited
+  * marking them as visited is cruical bc we'll be exploring every single cell
+  * so to avoid revisiting & recounting land as "new" we add each coordinate where we discover land to the set
+
+Thats it
+
+1. We can start by grabbing the dimensions of the input matrix
+2. we create out visit set & islands variable (this will serve as our count every time we discover a new island)
+
+3. For every row and column in that - each row if we find a position with land AND we haven't visited that position before then
+  - perform BFS on the current position
+  - when BFS is done - increment islands by 1
+  
+  3b) BFS 
+    - we create and initialize the queue
+    - add the current location to the visited set
+    - in the while-loop we grab our current location and calculate the possible directions
+      - iterate through all possible positions AND if the position is inbounds of the inputMatrix AND we haven't visited this position
+        - then we add this new position to our queue
+        - AND add it to the visit set
+        - then restart the whole process
+'''
+
 from collections import deque
 
 def numIslands(grid):
