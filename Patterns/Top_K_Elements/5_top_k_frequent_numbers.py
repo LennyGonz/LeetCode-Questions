1b845eeff143e9260cbfce9228014221c30e375c
lenny <lenny.gonzaleznyc@gmail.com>
2022-02-01 21:55:00 -0500

    Top K Frequent Numbers

--- Patterns/Top_K_Elements/5_top_k_frequent_numbers.py
+++ Patterns/Top_K_Elements/5_top_k_frequent_numbers.py
@@ -1,12 +1,51 @@
 '''
-Given an unsorted array of numbers, find the top ‘K’ frequently occurring numbers in it.
+LeetCode #347
+
+Given an unsorted array of numbers, find the top 'K' frequently occurring numbers in it.
 
 Input: [1, 3, 5, 12, 11, 12, 11], K = 2
 Output: [12, 11]
 Explanation: Both '11' and '12' apeared twice.
-'''
 
+we need to find the most frequently occurring number compared to finding the largest numbers.
+
+We can follow the same approach as discussed in the Top K Elements problem.
+
+However, in this problem, we first need to know the frequency of each number, for which we can use a HashMap. 
+
+Once we have the frequency map, we can use a Min Heap to find the 'K' most frequently occurring number. 
+
+In the Min Heap, instead of comparing numbers we will compare their frequencies in order to get frequently occurring numbers
+'''
 from heapq import *
+
+def find_k_frequent_numbers2(nums, k):
+  # our result list
+  topFrequentNumbers = []
+  
+  freqMap = {}
+  
+  # first we create the frequency map
+  for num in nums:
+    if num not in freqMap:
+      freqMap[num] = 0
+    freqMap[num] += 1
+  
+  # then we create the maxHeap
+  # again we negate the value so the numbers that occur the most are at the top of the heap
+  maxHeap = [(-val, key) for key,val in freqMap.items()]
+  heapify(maxHeap)
+  
+  # we iterate from 0 to k
+  # popping the root - k times
+  # because the smallest number is the root (since we negated the values in reality, the smallest number is the largest)
+  # but we don't care about the frequency value, we care about the number
+  # so we append each number to our result list: topFrequentNumbers
+  for _ in range(k):
+    topFrequentNumbers.append(heappop(maxHeap)[1])
+  
+  return topFrequentNumbers
+# -------------------------------------------------------------------- #
 from collections import Counter
 
 def find_k_frequent_numbers(nums, k):
@@ -21,24 +60,6 @@ def find_k_frequent_numbers(nums, k):
   for _ in range(k):
     topFrequentNumbers.append(heappop(maxHeap)[1])
 
-  return topFrequentNumbers
-
-def find_k_frequent_numbers2(nums, k):
-  topFrequentNumbers = []
-  
-  freqMap = {}
-  
-  for num in nums:
-    if num not in freqMap:
-      freqMap[num] = 0
-    freqMap[num] += 1
-  
-  maxHeap = [(-val, key) for key,val in freqMap.items()]
-  heapify(maxHeap)
-  
-  for _ in range(k):
-    topFrequentNumbers.append(heappop(maxHeap)[1])
-  
   return topFrequentNumbers
 
 def main():
