0ca018d5075c40cbd0390e392be7585ff9e9771e
lenny <lenny.gonzaleznyc@gmail.com>
2022-02-03 18:43:36 -0500

    valid bst

--- Code/graphs/valid_bst.py
+++ Code/graphs/valid_bst.py
@@ -1,28 +1,36 @@
 '''
-Validate A Binary search tree
-
-Time: O(n)
-Space: O(n)
--------------------------------
-
-Let's start with a simple definition of the nodes of a binary search tree!
-A parent node is greater than its left child but smaller than its right.
+LeetCode #98 - Validate A Binary search tree
 
     2
    / \
   1   3 // valid tree!
 
-    5
-   / \
-  1   4  // not valid here!
-     / \
-    3   6
+A valid BST is:
 
-One solution would be to check every parent as we work down the tree but it creates a lot 
-of complicated logic. So why don't we check every child on the way up! All we have to do 
-is keep track of the mininum and maximum valid values on the way down.
+1. The left subtree of a node contains only nodes with values LESS THAN the current node's value
+2. The right substree of a node contains only nodes with values GREATER than the current node's value
+3. Both the left and right subtree must also be BSTs
 
-To prevent some extra checks let's start with -infinity and infinity.
+so the strategy is
+
+we need to traverse every level of the tree and make sure that these conditions are true
+
+(rightNode.val > currentNode.val) and (leftNode.val < currentNode.val)
+
+We need to do this on every level and for every parent node, because we need to make sure that 
+every left subtree and right subtree is also a valid BST
+
+SO
+
+As we traverse each node we test the node for both of these conditions, if it fails either test, we immediately return False
+
+So when we traverse the left subtree: -infinity < currentNode.val < root.val
+root.val is the right bound
+-infinity is the left bound
+
+when traversing the right subtree: root.val < currentNode.val < infinity
+infinity is the right bound
+root.val is the left bound
 
 def isValidBST(self, root):
     return check_bst(root, float("-inf"), float("inf"))
@@ -63,6 +71,13 @@ def validateBinarySearchTree(root):
     return (validate(currentNode.left, lowerBound, currentNode.val) and validate(currentNode.right, currentNode.val, upperBound))
   
   return validate(root, float("-inf"), float("inf"))
+
+'''
+-----------------------
+Time: O(n)            |
+Space: O(n)           |
+-----------------------
+'''
 
 def main():
   root = TreeNode(2)
