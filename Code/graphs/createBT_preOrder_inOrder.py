f0fc18554933c03caa35669f8e99c0bce748ba2a
lenny <lenny.gonzaleznyc@gmail.com>
2022-02-03 18:43:22 -0500

    Create a BT from pre/inOrder traversal

--- Code/graphs/createBT_preOrder_inOrder.py
+++ Code/graphs/createBT_preOrder_inOrder.py
@@ -1,3 +1,33 @@
+'''
+LeetCode #105
+
+PreOrder is : root leftChild rightChild
+InOrder is : leftChild root rightChild
+
+So the first value in the preOrder list will always be the root, then the leftChild, then the rightChild
+  - so we have the first 3 nodes, but for the subsequent nodes we don't know 
+  - which is the leftChild and which is the rightChild for each parent node
+
+This is where the inOrder array comes in handy
+
+We use the inOrder array to distinguish which nodes belong in the left subtree and right subtree
+
+ex) PreOrder = [3,9,20,15,7] and InOrder = [9,3,15,20,7]
+
+IO = [9,3,15,20,7] -> our root is 3, we use preorder to determine that
+Notice how every value to the left of 3 is going to the left subtree 
+AND
+every value to the right of 3 is going to the right subtree
+
+Once we're done building the left subtree
+We move onto the right subtree
+
+InOrder = [15,20,7]
+We move to the middle element 20, because using the PreOrder traversal, we can determine that's the root of the right subtree
+to the left of 20 is 15 (leftChild)
+to the right of 20 is 7 (rightChild)
+'''
+
 class TreeNode:
   def __inite__(self, val=0,left=None,right=None):
     self.val = val
