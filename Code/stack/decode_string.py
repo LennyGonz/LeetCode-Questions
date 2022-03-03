c7bb8eb8c1f477bb2d8f14e47a9a2d74a83f52af
lenny <lenny.gonzaleznyc@gmail.com>
2022-03-02 23:56:56 -0500

    Stack - frequnecy map

--- Code/stack/decode_string.py
+++ Code/stack/decode_string.py
@@ -1,4 +1,44 @@
+'''
 # LeetCode Question 394
+
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. 
+
+Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc.
+
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k.
+
+For example, there will not be input like 3a or 2[4].
+
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+
+The idea is to seperate the number and the character
+We can do this by iterating through the string and appending every character to our stack UNLESS it is a closing bracket
+  - we don't want to append the closing bracket bc that's the character we'll use to stop and transform 3[a] into "aaa"
+  
+  so when we do reach a closing bracket - we stop appending to the stack
+  AND
+  we start popping what's on the stack - what should be at the top of the stack is a character - and we stop popping till we hit the opening bracket
+    so we have a variable that we initiate as an empty string - this will hold our decrypted string
+    our variable will hold this string
+  since we stopped popping when we hit an opening bracket we will need to do a single pop
+  
+  Once the single pop is done - what's left is an integer
+  Since our input is a string - we will need to cast our string into an integer - however the integer could be double/triple digits
+    so while the char is a digit we keep popping and rebuilding the integer
+  when we finally created the integer we cast it from a string to integer and multiply the char by the integer (3[a] -> 3*a = aaa)
+  then we add "aaa" back onto the stack
+  
+  We repeat this process until we've finished iterating over the entire string
+  And our return statement -> we join everything inside the stack
+'''
 
 def decodeString(s):
   stack = []
@@ -31,6 +71,12 @@ def decodeString(s):
       stack.append(int(k) * substr)
   
   return "".join(stack)
+
+'''
+Time: O(k * n) - We traverse a string of size n and iterate k times to decode each pattern of form k[string]
+Space: O(m+n) - where m is the number of letters(a-z) and n is the number of digits(0-9) in string s
+'''
+
 
 print(decodeString("3[a]2[bc]")) # "aaabcbc"
 print()
