566ebaa58518ff0b9d703d8c703653fe7f1ec011
lenny <lenny.gonzaleznyc@gmail.com>
2022-03-13 23:35:43 -0400

    Valid Parentheses

---
+++ Bloomberg/Remove_Invalid_Parentheses.py
@@ -0,0 +1,13 @@
+'''
+LeetCode #301
+
+Given a string s that contains parentheses and letters, remove the minimum number of invalid parentheses to make the input string valid.
+
+Return all the possible results. You may return the answer in any order.
+
+Input: s = "(a)())()"
+Output: ["(a())()","(a)()()"]
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+
+'''
